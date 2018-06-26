d0c6df2e260d9dd9908415000ecb48354b95b80a
tompng <tomoyapenguin@gmail.com>
2018-06-26 12:32:49 +0900

    refactor samples

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const strftime = require('strftime')
+app.listen(8080)
 
 // send time
 app.get('/time', (req, res) => {
@@ -25,16 +26,20 @@ app.get('/clock1', (req, res) => {
   }, 1000)
 })
 
-// clock (display:none, handling connection close)
+// clock(display: none)
 app.get('/clock2', (req, res) => {
-  res.write(`
-    <html>
-      <head>
-        <title>clock2</title>
-        <style>body{font-size:48px;}</style>
-      </head>
-      <body>
-  `)
+  res.write('<style>p{font-size:48px;text-align:center;}</style>')
+  let i = 0
+  setInterval(() => {
+    i++
+    res.write(`<p id='d${i}'>${strftime('%H:%M:%S', new Date)}</p>`)
+    res.write(`<style>#d${i-1}{display: none}</style>`)
+  }, 1000)
+})
+
+// clock with handling connection close
+app.get('/clock3', (req, res) => {
+  res.write('<style>body{font-size:48px;}</style>')
   let i = 0
   function sendTime() {
     const date = new Date
@@ -56,8 +61,30 @@ app.get('/clock2', (req, res) => {
 })
 
 // click with iframe
-let resObjects = []
+let message
 app.get('/click_action', (req, res) => {
+  message = req.query.q || JSON.stringify(req.query)
+  res.end(`ok(${message})`)
+})
+app.get('/click', (req, res) => {
+  res.write(`
+    <iframe name='ifrm' style='width:200px;height:40px;opacity:0.2'></iframe>
+    <a href='/click_action?q=ButtonA' target='ifrm'>ButtonA</a>
+    <a href='/click_action?q=ButtonB' target='ifrm'>ButtonB</a>
+    <a href='/click_action?q=ButtonC' target='ifrm'>ButtonC</a>
+    <form target='ifrm' action='click_action'>
+      <input type=image name=q style='border:2px solid red;width:100px;height:100px'>
+    </form>
+  `)
+  setInterval(() => {
+    res.write(`<p>${message}</p>`)
+  }, 1000)
+})
+
+
+// message with iframe
+let resObjects = []
+app.get('/message_action', (req, res) => {
   const message = req.query.q || JSON.stringify(req.query)
   res.end(`ok(${message})`)
   resObjects = resObjects.filter(res => {
@@ -66,17 +93,15 @@ app.get('/click_action', (req, res) => {
     else res.end()
   })
 })
-app.get('/click', (req, res) => {
+app.get('/message', (req, res) => {
   res.write(`
     <iframe name='ifrm' style='width:200px;height:40px;opacity:0.2'></iframe>
-    <a href='/click_action?q=Hello' target='ifrm'>Hello</a>
-    <a href='/click_action?q=JavaScript' target='ifrm'>JavaScript</a>
-    <a href='/click_action?q=World' target='ifrm'>World</a>
+    <a href='/message_action?q=Hello' target='ifrm'>Hello</a>
+    <a href='/message_action?q=JavaScript' target='ifrm'>JavaScript</a>
+    <a href='/message_action?q=World' target='ifrm'>World</a>
     <form target='ifrm' action='click_action'>
       <input type=image name=q style='border:2px solid red;width:100px;height:100px'>
     </form>
   `)
   resObjects.push(res)
 })
-
-app.listen(8080)
